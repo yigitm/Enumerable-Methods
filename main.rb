9cbfc3c7e88d76a4a1e1aca68f37d8d3c16f9cda
Aldana Abril Longhi <aldulonghi@gmail.com>
2020-07-16 13:00:46 -0300

    build my_each_with_index structure

--- main.rb
+++ main.rb
@@ -7,7 +7,11 @@ To built enumerables;
 2-Create two main section method & code-block
 3-loop the parameter according to length of object
 4-use the code block while the iteration to execute it
-5-return the output    
+5-return the output 
+
+#my_each_with_index:
+1. The behavior is similar to the #my_each method.
+2. This time the block is taking two parameters instead of one: element and number of index.
 =end
 
 module Enumerable
@@ -18,10 +22,22 @@ module Enumerable
         i += 1        
         end
     end
+
+    def my_each_with_index(obj)
+        result = my_each(obj) 
+        my_each_with_index(obj) 
+        puts my_each_with_index(obj)    
+    end
 end
+
+
 
 include Enumerable
 
 Enumerable.my_each([2,3,78]) {|item| 
     puts item + item
+}
+
+Enumerable.my_each_with_index([2,3,78]) {|item| 
+puts item + item
 }
